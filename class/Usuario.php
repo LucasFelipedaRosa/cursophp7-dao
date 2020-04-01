5d5d8728e3f91a9c4d82940c6d1fb10341f83fb1
unknown <you@example.com>
2020-04-01 14:00:07 -0300

    Metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -32,12 +32,22 @@
 			$this->dtcadastro = $value;
 		}
 
+		public function __toString(){
+
+			return json_encode(array(
+				"idusuario"=> $this->getIdusuario(),
+				"deslogin"=> $this->getDeslogin(),
+				"dessenha"=> $this->getDessenha(),
+				"dtcadastro"=> $this->getDtcadastro()->format("d/m/y H:i:s")
+			));
+		}
+
 		public function loadById($id){
 
 			$sql = new Sql();
 
 			$result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
-				":ID" => "3"
+				":ID" => $id
 			));
 
 			if(count($result) > 0){
@@ -50,14 +60,43 @@
 				$this->setDtcadastro(new DateTime($row['dtcadastro']));
 			}
 		}
-		public function __toString(){
 
-			return json_encode(array(
-				"idusuario"=> $this->getIdusuario(),
-				"deslogin"=> $this->getDeslogin(),
-				"dessenha"=> $this->getDessenha(),
-				"dtcadastro"=> $this->getDtcadastro()->format("d/m/y H:i:s")
+		public static function getList(){
+
+			$sql = new Sql();
+
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario");
+		}	
+
+		public static function Search($login){
+			$sql = new Sql();
+
+			return $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+				":SEARCH" => "%".$login."%"
 			));
+		}
+
+		public function login($login,$senha){
+			$sql = new Sql();
+
+			$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN and dessenha = :SENHA",array(
+				":LOGIN"=>$login,
+				":SENHA"=>$senha
+			));
+
+			if(count($result) > 0){
+
+				$row = $result[0];
+
+				$this->setIdusuario($row['idusuario']);
+				$this->SetDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));				
+			}else{
+
+				throw new Exception("Login e/ou senha invalidos.");
+				
+			}
 		}
 	}
 ?>
--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 	require_once("config.php");
 
-	$usuario= new Usuario();
+	//carrega 1 usuario
+	//$usuario= new Usuario();
+	//$usuario->loadById(3); 
+	//echo $usuario;
 
-	$usuario->loadById(3);
+	//carrega uma lista de usuarios]
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
+
+	//carrega uma lista de usuarios buscando pelo login
+	//$search = Usuario::search("Lu");
+	//echo json_encode($search);
+
+	//autentificação de login
+
+	$usuario = new Usuario();
+
+	$usuario->login("Lucasw","12345");
 
 	echo $usuario;
+
 ?>
